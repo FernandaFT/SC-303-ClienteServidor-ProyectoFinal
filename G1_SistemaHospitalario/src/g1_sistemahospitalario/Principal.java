da7ba37aad39dc235543430fa522b6dcfdbe15ce
FernandaFT <nanda199784@gmail.com>
2025-03-04 20:58:27 -0600

    adding proyect

---
+++ G1_SistemaHospitalario/src/g1_sistemahospitalario/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package g1_sistemahospitalario;
+
+/**
+ *
+ * @author fernandafajardo
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
