1c823693b6b4725b7582d542c6570e766417e872
FernandaFT <nanda199784@gmail.com>
2025-03-25 17:39:42 -0600

    Se agrego el JFrame Login,Registro, y la BD

--- G1_SistemaHospitalario/src/g1_sistemahospitalario/Principal.java
+++ G1_SistemaHospitalario/src/g1_sistemahospitalario/Principal.java
@@ -14,7 +14,10 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Login login = new Login();
+        login.setVisible(true);
+        login.setLocationRelativeTo(null);
     }
     
 }
